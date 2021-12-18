ad800610088fc28aa51c0765858eda80aedbfedd
Ega221 <Alekseenko.ea@edu.spbstu.ru>
2021-12-18 14:33:31 +0300

    Программа переработана

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -4,82 +4,52 @@
 
 using namespace std;
 
-bool compare(float main_x, float main_y, float x, float y)
+double find_cos(double main_x, double main_y, double x, double y)
 {
-    bool result = false;
-
-    if (main_y*x < y*main_x)
-    {
-        result = true;
-    }
-
-    return result;
-}
-
-double dot_prod(float main_x,float main_y,float x, float y) // функция скалярного произведения
-{
-    return (main_x*x+main_y*y)/(sqrt(x*x+y*y)*sqrt(main_x*main_x+main_y*main_y));
-}
-
-
-float get_coords_x(ifstream &input_file) // функция получения координаты x
-{
-    float x;
-    input_file >> x;
-    return x;
-}
-
-float get_coords_y(ifstream &input_file)  // функция получения координаты y
-{
-    float y;
-    input_file >> y;
-    return y;
+    return round((main_x * x + main_y * y) / (sqrt(x * x + y * y) * sqrt(main_y * main_y + main_x * main_x)) *
+                 10000000000.0) / 10000000000.0;
 }
 
 
 int main()
 {
-    float main_x;
-    float main_y;
-    double temp_dot_prod;
+    double main_x;
+    double main_y;
+    double cos;
     double leftmost_x = 0;
     double leftmost_y = 0;
-    double leftmost_cos = 2;
+    double leftmost_cos = 1;
     double rightmost_x = 0;
     double rightmost_y = 0;
-    double rightmost_cos = 2;
-    float x;
-    float y;
+    double rightmost_cos = 1;
+    double x;
+    double y;
 
-    string line;
 
     ifstream input_file("in.txt");
+    input_file >> main_x >> main_y;
 
-    main_x = get_coords_x(input_file);
-    main_y = get_coords_y(input_file);
-
-
-
-    while (getline(input_file, line))
+    while(input_file >> x >> y)
     {
-        x = get_coords_x(input_file);
-        y = get_coords_y(input_file);
-        temp_dot_prod = dot_prod(main_x, main_y, x, y);
-        if (compare(main_x, main_y, x, y) && temp_dot_prod <= leftmost_cos)
+        cos = find_cos(main_x, main_y, x, y);
+        if (main_y*x < main_x*y)
         {
-
-            leftmost_x = x;
-            leftmost_y = y;
-            leftmost_cos = temp_dot_prod;
-
-        } else if (temp_dot_prod <= rightmost_cos)
+            if (cos <= leftmost_cos)
+            {
+                leftmost_cos = cos;
+                leftmost_x = x;
+                leftmost_y = y;
+            }
+        } else if(cos <= rightmost_cos)
         {
+            rightmost_cos = cos;
             rightmost_x = x;
             rightmost_y = y;
-            rightmost_cos = temp_dot_prod;
         }
+
     }
     input_file.close();
+
 
     cout << "Leftmost: " << leftmost_x << ' ' << leftmost_y << endl;
     cout << "Rightmost: " << rightmost_x << ' ' << rightmost_y << endl;
